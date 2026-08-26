adb65737ad8dfbbffcc4e135c165a9652f1b1472
48h1337 <72185317+Zeref-XXX@users.noreply.github.com>
2026-08-26 12:15:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (12.41%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string str;
+        for (auto s : address) {
+            if (s == '.')
+                str += "[.]";
+            else
+                str += s;
+        }
+        return str;
+    }
+};
